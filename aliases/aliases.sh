e920b801a5c742f877e055dd47262392d9b96368
dizpunk <102290380+hatpunk@users.noreply.github.com>
2023-05-07 15:57:10 +0200

    Fix aliases tools path

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -5,18 +5,18 @@ chmod 644 aliases
 OS="$(uname)"
 
 # Generic aliases
-cat ./files/generic | tee -a aliases &> /dev/null
+cat ./tools/generic | tee -a aliases &> /dev/null
 
 # Exa / ls
 if command -v exa &> /dev/null; then
 
     echo -ne "\n" >> aliases
-    cat ./files/exa | tee -a aliases &> /dev/null
+    cat ./tools/exa | tee -a aliases &> /dev/null
 
 elif command -v ls &> /dev/null; then
 
     echo -ne "\n" >> aliases
-    source ./files/ls
+    source ./tools/ls
 
 fi
 
@@ -24,7 +24,7 @@ if [[ ${OS} == 'Linux' ]]; then
 
     # Generic linux aliases
     echo -ne "\n" >> aliases
-    cat ./files/linux | tee -a aliases &> /dev/null
+    cat ./tools/linux | tee -a aliases &> /dev/null
 
     # Distro specific aliases
     DISTRO="$( cat /etc/*-release | grep -Po '(?<=^ID=).*' )"
@@ -33,18 +33,18 @@ if [[ ${OS} == 'Linux' ]]; then
 
         # Arch based
         echo -ne "\n" >> aliases
-        cat ./files/arch | tee -a aliases &> /dev/null
+        cat ./tools/arch | tee -a aliases &> /dev/null
 
     elif [[ ${DISTRO} == 'fedora' ]]; then
 
         # Fedora based
         echo -ne "\n" >> aliases
-        cat ./files/fedora | tee -a aliases &> /dev/null
+        cat ./tools/fedora | tee -a aliases &> /dev/null
 
     elif [[ ${DISTRO} == 'debian' || ${DISTRO} == 'ubuntu' ]]; then
 
         # Debian based
         echo -ne "\n" >> aliases
-        cat ./files/debian | tee -a aliases &> /dev/null
+        cat ./tools/debian | tee -a aliases &> /dev/null
     fi
 fi
