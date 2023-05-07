c4566caef4d6ae745b1e8bf112235fd8e9b15bc1
dizpunk <102290380+hatpunk@users.noreply.github.com>
2023-05-07 16:36:57 +0200

    Fix aliases.sh output file

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -5,17 +5,17 @@ chmod 644 .aliases
 OS="$(uname)"
 
 # Generic aliases
-cat ./tools/generic | tee -a aliases &> /dev/null
+cat ./tools/generic | tee -a .aliases &> /dev/null
 
 # Exa / ls
 if command -v exa &> /dev/null; then
 
-    echo -ne "\n" >> aliases
-    cat ./tools/exa | tee -a aliases &> /dev/null
+    echo -ne "\n" >> .aliases
+    cat ./tools/exa | tee -a .aliases &> /dev/null
 
 elif command -v ls &> /dev/null; then
 
-    echo -ne "\n" >> aliases
+    echo -ne "\n" >> .aliases
     source ./tools/ls
 
 fi
@@ -23,8 +23,8 @@ fi
 if [[ ${OS} == 'Linux' ]]; then
 
     # Generic linux aliases
-    echo -ne "\n" >> aliases
-    cat ./tools/linux | tee -a aliases &> /dev/null
+    echo -ne "\n" >> .aliases
+    cat ./tools/linux | tee -a .aliases &> /dev/null
 
     # Distro specific aliases
     DISTRO="$( cat /etc/*-release | grep -Po '(?<=^ID=).*' )"
@@ -32,19 +32,19 @@ if [[ ${OS} == 'Linux' ]]; then
     if [[ ${DISTRO} == 'arch' || ${DISTRO} == 'manjaro' ]]; then
 
         # Arch based
-        echo -ne "\n" >> aliases
-        cat ./tools/arch | tee -a aliases &> /dev/null
+        echo -ne "\n" >> .aliases
+        cat ./tools/arch | tee -a .aliases &> /dev/null
 
     elif [[ ${DISTRO} == 'fedora' ]]; then
 
         # Fedora based
-        echo -ne "\n" >> aliases
-        cat ./tools/fedora | tee -a aliases &> /dev/null
+        echo -ne "\n" >> .aliases
+        cat ./tools/fedora | tee -a .aliases &> /dev/null
 
     elif [[ ${DISTRO} == 'debian' || ${DISTRO} == 'ubuntu' ]]; then
 
         # Debian based
-        echo -ne "\n" >> aliases
-        cat ./tools/debian | tee -a aliases &> /dev/null
+        echo -ne "\n" >> .aliases
+        cat ./tools/debian | tee -a .aliases &> /dev/null
     fi
 fi
